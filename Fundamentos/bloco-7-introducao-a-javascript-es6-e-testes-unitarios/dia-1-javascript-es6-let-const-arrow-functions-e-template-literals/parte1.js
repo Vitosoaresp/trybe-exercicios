06c83d3ebe22113873fc93a8b0433ca994630bf4
Vitor Soares <pereiravitor1218@gmail.com>
2022-03-03 18:47:18 -0300

    adiciona numero 2 e bonus

--- Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte1.js
+++ Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte1.js
@@ -1,3 +1,5 @@
+//1
+
 const testingScope = (escopo) => {
   if (escopo === true) {
     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
@@ -11,3 +13,27 @@ const testingScope = (escopo) => {
 }
 
 testingScope(true);
+
+//2
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const ordenar = () => {
+  oddsAndEvens[0] = 2;
+  oddsAndEvens[1] = 3;
+  oddsAndEvens[2] = 4;
+  oddsAndEvens[3] = 7;
+  oddsAndEvens[4] = 10;
+  oddsAndEvens[5] = 13;
+
+  return oddsAndEvens;
+}
+console.log(`Os números ${ordenar()} se encontram ordenados de forma crescente!`); 
+
+//bonus
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+const compareNumbers = (a, b) => a - b;
+
+const ordenar = () => oddsAndEvens.sort(compareNumbers);
+console.log(`Os números ${ordenar()} se encontram ordenados de forma crescente!`);
